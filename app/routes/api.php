47c6b69206d8cec888a8e80b83d149df2dad94be
cy322666 <cy322666@gmail.com>
2024-12-18 13:52:33 +0200

    build

---
+++ app/routes/api.php
@@ -0,0 +1,9 @@
+<?php
+
+use App\Http\Controllers\HookController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::post('payments/schedule', [HookController::class => 'schedule']);
+
+Route::post('payments/sum', [HookController::class => 'sum']);
