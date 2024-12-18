b43c789993218996258ab396f25f5b26f7d5e309
cy322666 <cy322666@gmail.com>
2024-12-18 15:58:05 +0200

    build

--- app/app/Http/Controllers/HookController.php
+++ app/app/Http/Controllers/HookController.php
@@ -11,12 +11,12 @@ class HookController extends Controller
     public function schedule(Request $request)
     {
         $transaction = Transaction::query()->create([
-            'type' => '',
-            'leads_count_last' => 1,
+            'type' => 'schedule',
+//            'leads_count_last' => 1,
 //            'leads_count_new' => 1,
-            'lead_id' => '',
-            'contact_id' => '',
-            'agreement' => '',
+            'lead_id' => $request->lead_id,
+            'contact_id' => $request->contact_id,
+            'agreement' => $request->agreement,
 //            'part_sum' => '',
 //        'all_sum' => '',
             'status' => 0,
