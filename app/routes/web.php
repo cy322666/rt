a77e4f286d01b9ad71bd37fb60b911a7c3213150
cy322666 <cy322666@gmail.com>
2024-12-18 13:47:57 +0200

    build

--- app/routes/web.php
+++ app/routes/web.php
@@ -1,8 +1,13 @@
 <?php
 
 use App\Http\Controllers\DocController;
+use App\Http\Controllers\HookController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('docs/get',  [DocController::class => 'get']);
 
 Route::get('docs/push', [DocController::class => 'push']);
+
+Route::post('payments/schedule', [HookController::class => 'schedule']);
+
+Route::post('payments/sum', [HookController::class => 'sum']);
